cc4bf57cc8800fd12d2d9fe1b530d662fb3dd3d6
BitcoinKing <nanangassa@yahoo.fr>
2020-08-17 22:05:37 -0400

    * .gitignore: Initial commit User.cs
    * p2pCryptos.sln:
    * Program.cs:
    * Startup.cs:
    * User.cs:
    * appsettings.json:
    * p2pCryptos.csproj:
    * site.js:
    * favicon.ico:
    * site.css:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * Privacy.cshtml:
    * Error.cshtml:
    * _ViewImports.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * UserController.cs:
    * HomeController.cs:
    * LICENSE:
    * LICENSE.txt:
    * launchSettings.json:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.js.map:
    * bootstrap.min.js:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * bootstrap.bundle.js.map:
    * bootstrap-grid.min.css:
    * bootstrap-grid.css.map:
    * LICENSE.txt:
    * bootstrap.bundle.min.js:
    * jquery.validate.js:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * jquery.validate.min.js:
    * bootstrap.bundle.min.js.map:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ p2pCryptos/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace p2pCryptos.Controllers
+{
+    public class UserController
+    {
+        public UserController()
+        {
+        }
+    }
+}
---
+++ p2pCryptos/Models/User.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
+
+namespace p2pCryptos.Models
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+
+        [Key]
+        [Required]
+        public long Userid { get; set; }
+
+        //[ForeignKey("Standard")]
+        //[Required]
+        public string Roleid { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Firstname { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Lastname { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Emailaddress
+        { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.Password)]
+        public string Password
+        { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Dateofbirth { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string City { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Postalcode { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Country { get; set; }
+    }
+}
+
